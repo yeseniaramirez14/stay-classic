d7fb1289ed6c644f038a53c0fd99af684b67869f
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-05-09 17:18:21 -0500

    working

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Technician(models.model):
+    name = models.CharField(max_length=50)
+    employee_number = models.PositiveSmallIntegerField()
+
+class Service(models.model):
+    vin = models.CharField(max_length=50, unique=True)
+    customer = models.CharField(max_length=50)
+    date = models.DateField()
+    time = models.TimeField()
+    technician = models.ForeignKey(
+        Technician,
+        related_name="technician",
+        on_delete=models.PROTECT,
+    )
