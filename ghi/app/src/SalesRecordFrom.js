8b7cdac249b8b7540160def073cfbe7e373698eb
Corey Edwards <coreydanieledwards@gmail.com>
2022-05-11 11:52:31 -0700

    salesrecordsform troubleshooting v.04

--- ghi/app/src/SalesRecordFrom.js
+++ ghi/app/src/SalesRecordFrom.js
@@ -1,74 +1,84 @@
 import React from 'react';
 
 class SalesRecordForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            vins: [],
-            salesReps: [],
-            customers: [],
-            price: "",
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {        
+      super(props);        
+      this.state = {            
+          automobile: '',
+          salesRep: '',
+          customer: '', 
+          price: '',
+          salesReps: [],
+          autos: [],
+          customers: [],           
+      }
 
-    handleChange(event) {
-        const newState = {}
-        newState[event.target.id] = event.target.value;
-        // console.log(event)
-        this.setState(newState)
-    }
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    // As an example to reference
-    // handleCustomerChange(event) {
-    //     const value = event.target.value;
-    //     this.setState({manufactuer: value})
-    // }
 
     async componentDidMount() {
-        const vinsURL = "http://localhost:8090/api/salesrecords/";
-        const salesrepsURL = "http://localhost:8090/api/salesrecords/";
-        const customersURL = "http://localhost:8090/api/salesrecords/";
+      const salesUrl = "http://localhost:8090/api/salesreps/";
+      const customerUrl = "http://localhost:8090/api/customers/";
+      const autoUrl = "http://localhost:8100/api/automobiles/";
 
-        const response = await fetch(url);
-    
-        if (response.ok) {
-          const data = await response.json();
-          this.setState({ salesrecords: data.salesrecords })
-          }
+
+        const salesResponse = await fetch(salesUrl);
+        const customerResponse = await fetch(customerUrl);
+        const autoResponse = await fetch(autoUrl);
+
+        if (autoResponse.ok && customerResponse.ok && autoResponse.ok) {
+            const saleData = await salesResponse.json();
+            const customerData = await customerResponse.json();
+            const autoData = await autoResponse.json();
+            this.setState({ 
+                salesReps: saleData.salesreps,
+                customers: customerData.customers,
+                autos: autoData.autos
+             })
         }
+    }  
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        const data = {...this.state};
-        data.date_time = data.dateTime
-        delete data.dateTime
-        delete data.technicians
-        console.log(data)
 
-        const serviceURL = "http://localhost:8090/api/salesrecords/";
-        const fetchConfig = {
+        async handleSubmit(event) {
+          event.preventDefault();
+          const data = {...this.state};
+          data.sales_rep = data.salesRep;        
+          delete data.salesReps;
+          delete data.salesRep;
+          delete data.customers;               
+          delete data.autos;               
+          console.log(data)
+      
+          const salesRecordsUrl = 'http://localhost:8090/api/sales/';
+          const fetchConfig = {
             method: "post",
             body: JSON.stringify(data),
             headers: {
-                'Content-Type': 'application/json'
+              'Content-Type': 'application/json',
             },
-        };
-        const response = await fetch(serviceURL, fetchConfig);
-        if (response.ok) {
-            const newService = await response.json();
-            console.log(newService)
-
+          };
+          const response = await fetch(salesRecordsUrl, fetchConfig);
+          if (response.ok) {
+            const newName = await response.json();
+            console.log(newName)          
             const cleared = {
-                vin: '',
-                customer: '',
-                dateTime: '',
-                reason: '',
-            };
+              salesRep: '',
+              automobile: '',
+              customer: '',
+              price: '',
+            }
             this.setState(cleared);
+          }
         }
-    }
+  
+      handleChange(event) {
+          const newState = {};
+          newState[event.target.id] = event.target.value;
+          this.setState(newState);
+      }
+  
 
     render() {
         return (
@@ -77,18 +87,29 @@ class SalesRecordForm extends React.Component {
               <div className="shadow p-4 mt-4">
                 <h1>Create a new Sale Record</h1>
                 <form onSubmit={this.handleSubmit} id="create-salesrecord-form">
-                  <div className="mb-3">
-                      <select onChange={this.handleChange} value={this.state.vins} required className="form-select" id="vin">
-                        <option value="">Choose a VIN number</option>
-                        {this.state.vin.map(vin => {
-                          console.log(vin)
-                          return (
-                            <option key={vin} value={vin}>{vin}</option>
-                          )
-                        })}
-                      </select>
-                  </div>
-                  <div className="form-floating mb-3">
+                <div className="mb-3">
+                        <select onChange={this.handleChange} value={this.state.automobile} required name="automobile" id="automobile" className="form-select">
+                          <option value="">Choose an automobile</option>
+                          {this.state.autos.map(auto => {
+                            return (
+                              <option key={auto.href} value={auto.vin}>
+                                  {auto.year} {auto.color} {auto.model.manufacturer.name} {auto.model.name}
+                              </option>
+                            )
+                          })}
+                        </select>
+                      </div>
+                <div className="mb-3">
+                        <select onChange={this.handleChange} value={this.state.salesReps} required name="salesRep" id="salesRep" className="form-select">
+                          <option value="">Choose a sales rep</option>
+                          {this.state.salesReps.map(salesrep => {
+                            return (
+                              <option key={salesrep.name} value={salesrep.name}>{salesrep.name}</option>
+                            )
+                          })}
+                        </select>
+                      </div>
+                <div className="form-floating mb-3">
                     <input onChange={this.handleChange} value={this.state.price} placeholder="price" type="text" id="price" className="form-control" />
                     <label htmlFor="price">Price</label>
                   </div>
