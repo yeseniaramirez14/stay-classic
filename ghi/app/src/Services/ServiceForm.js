e6d7fd19dfd3ed69fe875abbbc5202a7de9de3b8
Yesenia Ramirez <yeseniaramirez414@gmail.com>
2022-05-13 12:26:00 -0500

    successful message added to services forms

--- ghi/app/src/Services/ServiceForm.js
+++ ghi/app/src/Services/ServiceForm.js
@@ -10,6 +10,7 @@ class ServiceForm extends React.Component {
             technicians: [],
             technician: '',
             reason: '',
+            successfulSubmit: false,
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -38,6 +39,7 @@ class ServiceForm extends React.Component {
         data.date_time = data.dateTime
         delete data.dateTime
         delete data.technicians
+        delete data.successfulSubmit
         console.log(data)
 
         const serviceURL = "http://localhost:8080/api/services/";
@@ -52,6 +54,7 @@ class ServiceForm extends React.Component {
         if (response.ok) {
             const newService = await response.json();
             console.log(newService)
+            this.state.successfulSubmit = true;
 
             const cleared = {
                 vin: '',
@@ -65,12 +68,19 @@ class ServiceForm extends React.Component {
     }
 
     render() {
+      let formClasses = '';
+      let alertClasses = 'alert alert-success d-none mb-0'
+
+      if (this.state.successfulSubmit) {
+        formClasses='d-none';
+        alertClasses='alert alert-success mb-0'
+      }
         return (
             <div className="row">
             <div className="offset-3 col-6">
               <div className="shadow p-4 mt-4">
                 <h1>Create a New Service Appointment</h1>
-                <form onSubmit={this.handleSubmit} id="create-service-form">
+                <form onSubmit={this.handleSubmit} id="create-service-form" className={formClasses}>
                   <div className="form-floating mb-3">
                     <input onChange={this.handleChange} value={this.state.vin} placeholder="Vin" required type="text" id="vin" className="form-control" />
                     <label htmlFor="vin">Vin number</label>
@@ -99,6 +109,9 @@ class ServiceForm extends React.Component {
                   </div>
                   <button className="btn btn-primary">Create</button>
                 </form>
+                <div className={alertClasses} id="success-message">
+                  You have created a new service appointment. 
+                </div>
               </div>
             </div>
           </div>
--- ghi/app/src/Services/TechnicianForm.js
+++ ghi/app/src/Services/TechnicianForm.js
@@ -6,6 +6,7 @@ class TechnicianForm extends React.Component {
         this.state = {
             name: '',
             employeeNumber: '',
+            successfulSubmit: false,
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,6 +26,7 @@ class TechnicianForm extends React.Component {
         data.employee_number = data.employeeNumber
         delete data.employeeNumber
         delete data.technicians
+        delete data.successfulSubmit;
         console.log(data)
 
         const technicianURL = "http://localhost:8080/api/technicians/";
@@ -40,6 +42,7 @@ class TechnicianForm extends React.Component {
         if (response.ok) {
             const newService = await response.json();
             console.log(newService)
+            this.state.successfulSubmit = true;
 
             const cleared = {
                 name: '',
@@ -50,12 +53,19 @@ class TechnicianForm extends React.Component {
     }
 
     render() {
+      let formClasses = '';
+      let alertClasses = 'alert alert-success d-none mb-0'
+
+      if (this.state.successfulSubmit) {
+        formClasses='d-none';
+        alertClasses='alert alert-success mb-0'
+      }
         return (
             <div className="row">
               <div className="offset-3 col-6">
                 <div className="shadow p-4 mt-4">
                   <h1>Create a New Technician</h1>
-                  <form onSubmit={this.handleSubmit} id="create-technician-form">
+                  <form onSubmit={this.handleSubmit} id="create-technician-form" className={formClasses}>
                     <div className="form-floating mb-3">
                       <input onChange={this.handleChange} value={this.state.name} placeholder="Name" required type="text" id="name" className="form-control" />
                       <label htmlFor="name">Name</label>
@@ -66,6 +76,9 @@ class TechnicianForm extends React.Component {
                     </div>
                     <button className="btn btn-primary">Create</button>
                   </form>
+                  <div className={alertClasses} id="success-message">
+                  You have created a new technician. 
+                </div>
                 </div>
               </div>
             </div>
