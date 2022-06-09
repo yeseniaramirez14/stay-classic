5aa49354aba6a2563a62f5ab3e32a9dd64ecce07
Corey Edwards <coreydanieledwards@gmail.com>
2022-06-09 15:14:13 -0700

    i need to pull/ working on connections

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,3 +1,11 @@
+import Card from "./Card";
+import content from "./content";
+
+function createEntry(entry) {
+  return <Card key={entry.id} emoji={entry.emoji} back={entry.back} />;
+}
+
+
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
@@ -7,6 +15,7 @@ function MainPage() {
           The premiere solution for automobile dealership
           management!
         </p>
+        {content.map(createEntry)}
       </div>
     </div>
   );
